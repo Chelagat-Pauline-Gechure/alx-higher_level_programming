fd0db1ef5911a5c89ca3944ae72be9ef46fb7c7b
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-06-08 12:01:20 -0700

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
-i, addResult = 1, 0
+integer, addResult = 1, 0
 
 if __name__ == "__main__":
-    while i < len(argv):
-        addResult += int(argv[i])
-        i += 1
+    while integer < len(argv):
+        addResult += int(argv[integer])
+        integer += 1
     print(addResult)
