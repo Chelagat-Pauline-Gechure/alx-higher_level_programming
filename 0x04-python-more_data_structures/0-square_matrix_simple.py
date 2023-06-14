2a3b6278e2f1722566da5fb72cd5a84cf11e903b
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-06-14 00:57:17 -0700

    0. Squared simple with no errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    #Create a new matrix with same size as input
-    result = [[0]* len(row) for row in matrix]
+    # Create a new matrix with same size as input
+    result = [[0]*len(row) for row in matrix]
 
-    #Computing the square value for each integer
+# Computing the square value for each integer
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             result[i][j] = matrix[i][j]**2
-    return  result
+    return result
