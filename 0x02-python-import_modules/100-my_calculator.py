a8f90dc4f96af8f56f8f7c577705e79da691a44f
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-06-21 12:03:12 -0700

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-    if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
-
-    operation = argv[2]
-    op_function = {'+': add, '-': sub, '*': mul, '/': div}
-    if operation not in op_function:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    print('{:d} {:s} {:d} = {:d}'.format(a, operation, b, op_function[operation](a, b)))
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
