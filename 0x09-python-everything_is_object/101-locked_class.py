e744d3886520a99af5516bcffb356804314dc5a6
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-04 11:02:19 -0700

    30. Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Contains 'LockedClass' class.
+"""
+
+
+class LockedClass:
+    """
+    Defines LockedClass that creates specific classes and object attributes.
+    """
+    __slots__ = ["first_name"]
