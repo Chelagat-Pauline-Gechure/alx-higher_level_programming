4d71e7a12aff0e89cc1651e9980c395734f9a5ec
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-10 15:11:01 -0700

    10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,18 +4,15 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
+class Square(Rectangle):
     """ Define the Square class """
 
     def __init__(self, size):
         """ Initialize square method """
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """ Compute & return area of square """
         return self.__size * self.__size
-
-    def __str__(self):
-        """Returns a string"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
