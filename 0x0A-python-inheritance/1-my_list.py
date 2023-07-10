7c9269db752b097a71ffdda15ae115561b04c53f
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-10 10:37:26 -0700

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" 1-my_list.py defines a class 'MyList' that inherits from superclass 'list' """
+
+class MyList(list):
+    """ Implement sorting & printing of built-in list class """
+
+    def print_sorted(self):
+        print(sorted(self))
