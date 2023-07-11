5c92ed42e3131dfaa6db89e1c6179918e1eb1e08
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-11 15:38:02 -0700

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module 9-student
+defines a class Student
+"""
+
+
+class Student:
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student using constructor method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary representation of the Student
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            diction = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    diction[att] = self.__dict__[att]
+            return diction
