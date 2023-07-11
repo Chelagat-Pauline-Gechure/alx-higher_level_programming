30f098b63af86af7fd00f302d2ee453ceb2b7054
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-11 16:00:22 -0700

    13. Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,7 +8,7 @@ after each line containing a specific string
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    function that inserts a line of text to a file, 
+    function that inserts a line of text to a file,
     after each line containing a specific string
     """
 
