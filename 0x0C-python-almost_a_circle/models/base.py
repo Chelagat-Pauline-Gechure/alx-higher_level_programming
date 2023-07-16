58487dc05bac0572a0dd57372e1a263b37baf7c5
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-16 10:51:54 -0700

    Created Base class which will manage id attribute of all future classes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+class Base:
+    """ This class will be the “base” of all other classes in this project.
+    It's goals is to manage all "id" attribute of future classes. """
+    __nb_objects = 0
+
+    def __init__(self, id=None);
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
