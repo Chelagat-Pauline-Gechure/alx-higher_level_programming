a0548395e413a63a24be77b863c8f478363f0cb5
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-16 21:33:31 +0300

    2. Filter states by user input Like binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa where name matches an argument
+"""Lists all states from the database hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    username: str = sys.argv[1]
-    password: str = sys.argv[2]
-    db_name: str = sys.argv[3]
-    name_of_state: str = sys.argv[4]
-    host: str = "localhost"
-    port: int = 3306
-    sql_query = "SELECT * FROM states WHERE name LIKE BINARY\
-    '{}' ORDER BY id ASC".format(name_of_state)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    query = "SELECT * FROM states WHERE name LIKE BINARY\
+ '{}' ORDER BY id ASC".format(state_name)
 
     db = MySQLdb.connect(
+        host="localhost",
         user=username,
-        host=host,
-        port=port,
         password=password,
         database=db_name,
+        port=3306,
     )
     cursor = db.cursor()
 
-    cursor.execute(sql_query)
-    rows = cursor.fetchall()
-    for row in rows:
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
         print(row)
+    cursor.close()
+    db.close()
