3cb58fa454d5673ccc39e0b6dc1c51a899b22c1a
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-17 22:32:37 +0300

    15. City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define a State class and create a connection to the database.
+"""
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+"""Create an instance of declarative base"""
+Base = declarative_base()
+
+
+class State(Base):
+    """Define the State class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City", 
+        cascade="all, delete",
+        backref='state'
+    )
