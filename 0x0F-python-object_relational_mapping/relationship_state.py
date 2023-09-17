8abf2170e322f5e38a8934128591a9e3389bdecf
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-17 22:44:48 +0300

    15. City relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,7 +18,7 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship(
-        "City", 
+        "City",
         cascade="all, delete",
         backref='state'
     )
