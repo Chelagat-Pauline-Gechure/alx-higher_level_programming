5a2e972837f26087e90f80168857f98e3c6d0d33
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-17 17:26:53 +0300

    7. All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,15 +11,20 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+
+    """SQLAlchemy database engine created with pooling & pre-ping"""
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             username, password, db_name
         ),
         pool_pre_ping=True,
     )
+    """Establish connection to the database"""
     with engine.connect() as connection:
         query = select(State).order_by(State.id.asc())
         states = connection.execute(query)
+
+        """Iterate over results & display them"""
         for state in states:
             print(f"{state.id}: {state.name}")
 
