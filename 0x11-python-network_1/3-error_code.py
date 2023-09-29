cf179fb0caaca36e87d6c332d94e68b6fa8a7d58
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-29 14:14:21 -0700

    fized error in task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,6 +2,7 @@
 """ sends a request to the URL and displays the body of the response"""
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from sys import argv
 
 
 if __name__ == "__main__":
