3791aef72bcefd0bce31b4da3e12278162e19bfb
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-29 15:03:16 -0700

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,12 @@
+
 #!/usr/bin/python3
-""" Holberton School Backend Interview """
+""" Holberton Backend Challenge """
 import requests
 from sys import argv
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits"\
-          .format(argv[2], argv[1])
-    req = requests.get(url)
-    number = 0
-    for i in req.json():
-        if n < 10:
-            print("{}: {}".format(i.get("sha"),
-                  i.get("commit").get("author").get("name")))
-        number += 1
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
