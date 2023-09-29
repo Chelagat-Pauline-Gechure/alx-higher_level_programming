3959c76acf610cf5c6bc713d16a27237ddf7f501
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-09-29 14:11:37 -0700

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Displays the body of the response"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
