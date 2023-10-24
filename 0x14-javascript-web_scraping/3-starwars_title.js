a90ec5e7c70ba8a5896579fd997a7feb8af48460
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-24 19:10:40 +0300

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
+if (process.argv.length !== 3) {
+    console.error('Usage: node <script-file> <ID>');
+    process.exit(1);
+  }
 const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
