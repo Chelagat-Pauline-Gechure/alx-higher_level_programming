85f33a71f4c9feb6e61e2fba0f76304e4c4da821
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-24 08:52:58 -0700

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-// Check if the correct number of command-line arguments is provided
-if (process.argv.length !== 3) {
-  console.error('Usage: node 4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(`HTTP Status Code: ${response.statusCode}`);
+    console.log(error);
   } else {
-    const filmsData = JSON.parse(body);
-    const characterId = 18; // Wedge Antilles' character ID
-    const count = filmsData.results.reduce((total, film) => {
-      return total + (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`) ? 1 : 0);
-    }, 0);
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
     console.log(count);
   }
 });
