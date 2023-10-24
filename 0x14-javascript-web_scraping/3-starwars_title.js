b6fafdd92865112233c4bf7bdffb24a8de49bd45
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-24 19:04:40 +0300

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(url, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
