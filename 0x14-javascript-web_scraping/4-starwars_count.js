14ac2fda0797c1501c8e6b22010edfc1e85b6402
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-24 19:16:32 +0300

    tASK 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
+const apiUrlurl = process.argv[2];
 const characterId = '18';
 let count = 0;
 
-request.get(url, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
