45b9348c263895e1d36d79cc81a529919b417da6
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-24 18:46:39 +0300

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-// Check if the file path argument and string to write is provided
-if (process.argv.length !== 3) {
-  console.error('Usage: node <script-file> <ID>');
-  process.exit(1);
-  }
-
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request.get(apiUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.log('Error',error);
+    console.log(error);
   } else {
-    const movieData = JSON.parse(body);
-    console.log(movieData.title);
+    const data = JSON.parse(body);
+    console.log(data.title);
   }
 });
