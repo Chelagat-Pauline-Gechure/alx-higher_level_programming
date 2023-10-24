0321a90fe201b071f4583406805f86be5b52ed99
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-24 07:50:06 -0700

    1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if correct number of command-line arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file_path> "<string_to_write>"');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+	if (err) {
+		console.error(err);
+	}
+});
